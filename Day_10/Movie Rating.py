09f09aa2d96a7d43a89960548839b2d2a5ad0304
vijayabhaskarvb <vijayabhaskarvb1310@gmail.com>
2025-07-18 19:34:25 +0530

    Day 10 Task

---
+++ Day_10/Movie Rating.py
@@ -0,0 +1,13 @@
+Star = int(input("Enter the Movie Rating: "))
+if Star == 5:
+    print("Excellent")
+elif Star == 4:
+    print("Very Good")
+elif Star == 3:
+    print("Good")
+elif Star == 2:
+    print("Average")
+elif Star == 1:
+    print("Not Satisfied")
+else:
+    print("Invalid")
